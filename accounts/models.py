13fa82643b1659ba2986ceb23a15356dae6299f3
Hiding <s.a.e.i.d@live.com>
2023-06-25 17:19:58 +0000

    1sh

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not username:
+            raise ValueError('Users must have an username')
+        if not email:
+            raise ValueError('Users must have an email')
+
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username,  email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        user = self.create_user(username, email, password=password, **extra_fields)
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(verbose_name='Username', max_length=255, unique=True)
+    email = models.EmailField(verbose_name='email', max_length=255, unique=True)
+    phone = models.BigIntegerField(verbose_name='Phone Number', unique=True, blank=True, null=True)
+    is_active = models.BooleanField(default=True, verbose_name="is_active")
+    is_staff = models.BooleanField(default=False, verbose_name="is_staff")
+    is_superuser = models.BooleanField(default=False, verbose_name="is_superuser")
+
+    objects = UserManager()
+
+    # We can use any field for username
+    USERNAME_FIELD = 'username'  # or 'email' or 'phone'
+    REQUIRED_FIELDS = ['username', 'email']
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from django.utils.translation import gettext_lazy as _
+from accounts.forms import LoginForm, RegisterForm
+from django.contrib import messages
+from accounts.models import User
+
+
+def userRegister(request):
+    form = RegisterForm()
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            if not User.objects.filter(email=cd['username']).exists():
+                if not User.objects.filter(email=cd['email']).exists():
+                    user = User.objects.create_user(
+                        username=cd['username'], phone=cd['phone'], email=cd['email'], password=cd['password1'])
+                    user.save()
+                    login(request, user)
+                    messages.success(request, _("You successfully registered a user"), extra_tags="success")
+                    return redirect('crm:home')
+                else:
+                    messages.error(request, _("This Email is exists"), extra_tags="warning")
+            else:
+                messages.error(request, _("This Username is exists"), extra_tags="warning")
+        else:
+            import json
+            er = json.loads(form.errors.as_json())
+            for e in er:
+                messages.error(request, er[e][0]['message'], 'warning')
+    return render(request, 'accounts/register.html', {'form': form})
+
+
+def userLogin(request):
+    if not request.user.is_active:
+        if request.method == 'POST':
+            form = LoginForm(request.POST)
+            if form.is_valid():
+                cd = form.cleaned_data
+                if User.objects.filter(username=cd['username']).exists():
+                    user = authenticate(request, username=cd['username'], password=cd['password'])
+                    if user is not None:
+                        login(request, user)
+                        messages.success(request, _("logged in successfully"), extra_tags="success")
+                        return redirect('crm:home')
+                    else:
+                        messages.error(request, _("your username Or Password is wrong"), extra_tags="warning")
+                else:
+                    messages.error(request, _("No account created with this username"), extra_tags="warning")
+                    return redirect('accounts:login')
+            else:
+                messages.error(request, _("Please enter your information correctly"), extra_tags="warning")
+        else:
+            form = LoginForm()
+        return render(request, 'accounts/login.html', {'form': form})
+    else:
+        return redirect('crm:home')
+
+
+@login_required()
+def LogoutPage(request):
+    logout(request)
+    messages.success(request, _("You Logged Out successfully"), extra_tags="success")
+    return redirect('crm:home')
