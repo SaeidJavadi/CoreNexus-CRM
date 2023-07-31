46d2048ce1d821bc3d09f34ec917511c3196949b
Hiding <s.a.e.i.d@live.com>
2023-07-31 12:38:46 +0330

    np firebase

--- api/views.py
+++ api/views.py
@@ -257,19 +257,19 @@ class NotificationViewSet(ModelViewSet):
         return Response({'message': 'Error to Update.', 'error': serializer.errors}, 400)
 
 ######## FireBase ########
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import messaging
+# import firebase_admin
+# from firebase_admin import credentials
+# from firebase_admin import messaging
 
-cred = credentials.Certificate("<path-to-your-firebase-credentials-json>")
-firebase_admin.initialize_app(cred)
+# cred = credentials.Certificate("<path-to-your-firebase-credentials-json>")
+# firebase_admin.initialize_app(cred)
 
-def send_notification(user_token, title, body):
-    message = messaging.Message( notification=messaging.Notification(title=title, body=body),token=user_token)
-    response = messaging.send(message)
-    print('Successfully sent message:', response)
+# def send_notification(user_token, title, body):
+#     message = messaging.Message( notification=messaging.Notification(title=title, body=body),token=user_token)
+#     response = messaging.send(message)
+#     print('Successfully sent message:', response)
 
-user_token = "<user-fcm-token>"
-title = "New Notification"
-body = "You have a new notification!"
-send_notification(user_token, title, body)
+# user_token = "<user-fcm-token>"
+# title = "New Notification"
+# body = "You have a new notification!"
+# send_notification(user_token, title, body)
