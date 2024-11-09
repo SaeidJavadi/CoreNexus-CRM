d089d69b62fe10714218d575f541185b5a9db657
Saeid Javadi <S.Javadi.P@gmail.com>
2024-11-09 20:56:55 +0330

    np

--- base/settings.py
+++ base/settings.py
@@ -68,7 +68,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.locale.LocaleMiddleware'
+    'django.middleware.locale.LocaleMiddleware',
+    'allauth.account.middleware.AccountMiddleware'
 ]
 
 ROOT_URLCONF = 'base.urls'
